ef269411c1de89a6c9812a707927c557c20e5ed4
reza <rezafinola@gmail.com>
2022-03-10 21:10:24 +0700

    [ADD] DogsDao

--- app/src/main/java/com/rezzavinola/dogapplication/data/local/DogsDao.kt
+++ app/src/main/java/com/rezzavinola/dogapplication/data/local/DogsDao.kt
@@ -1,4 +1,18 @@
 package com.rezzavinola.dogapplication.data.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.rezzavinola.dogapplication.data.model.entity.DogsEntity
+import retrofit2.http.Query
+
+@Dao
 interface DogsDao {
+
+    @Query("select * from dogs")
+    fun getAllDogs(): List<DogsEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllDogs(dogs: List<DogsEntity>)
+
 }
