72fe7f2066fc2f016639b7056bed99d5828e1638
reza <rezafinola@gmail.com>
2022-03-11 08:57:35 +0700

    [ADD] DogsEntity

--- app/src/main/java/com/rezzavinola/dogapplication/data/model/entity/DogsEntity.kt
+++ app/src/main/java/com/rezzavinola/dogapplication/data/model/entity/DogsEntity.kt
@@ -1,4 +1,15 @@
 package com.rezzavinola.dogapplication.data.model.entity
 
-class DogsEntity {
-}
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+
+@Entity(tableName = "dogs")
+@Parcelize
+data class DogsEntity (
+    @PrimaryKey (autoGenerate = true) var id: Long = 0,
+    @ColumnInfo(name = "image_url") var imageUrl: String,
+    ): Parcelable
