88f168f42c0c009485f58f6af5f36942e5690457
reza <rezafinola@gmail.com>
2022-03-13 13:59:31 +0700

    DogsDatabase

--- app/src/main/java/com/rezzavinola/dogapplication/data/local/DogsDatabase.kt
+++ app/src/main/java/com/rezzavinola/dogapplication/data/local/DogsDatabase.kt
@@ -1,7 +1,35 @@
 package com.rezzavinola.dogapplication.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.rezzavinola.dogapplication.data.model.entity.DogsEntity
 
-abstract class DogsDatabase ()
+
+@Database(entities = [DogsEntity::class], version = 1)
+abstract class DogsDatabase : RoomDatabase() {
+
+    abstract fun dogs(): DogsDao
+
+    companion object {
+        private var database: DogsDatabase? = null
+
+        fun instance(context: Context): DogsDatabase {
+            if (database == null) {
+                synchronized(DogsDatabase::class) {
+                    database = Room.databaseBuilder(
+                        context.applicationContext,
+                        DogsDatabase::class.java,
+                        "dog_database.db"
+                    ).build()
+                }
+            }
+            return database!!
+        }
+
+        fun destroyInstance() {
+            database = null
+        }
+    }
+}
